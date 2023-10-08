6f48d34c0547ca0664fef497b550b85fdd8ccfc6
DiegoMerida28 <145516421+DiegoMerida28@users.noreply.github.com>
2023-10-07 22:23:27 -0600

    Agregar archivos de proyecto.

---
+++ Arreglo de aves-Python/Arreglo_de_aves_Python.py
@@ -0,0 +1,17 @@
+aves = ["Loro gris, ", "Paloma de diamante, ", "Cóctel "]
+
+print("Los valores de la lista antes de insertar: ")
+
+for ave in aves:
+    print(ave, end=" ")
+print("\n")
+
+aves.append("Mayna, ")
+aves.append("Periquitos, ")
+aves.append("Cacatúa, ")
+
+print("Los valores de la lista después de insertar: ")
+
+for ave in aves:
+    print(ave, end=" ")
+print ("\n")
